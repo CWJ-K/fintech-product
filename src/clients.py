4ecf3cf64aaeac5c767f1742ad1596b5fcbf7fae
Louise <wanju412@gmail.com>
2022-05-30 11:22:45 +0000

    fix(clients): typo enginer

--- src/clients.py
+++ src/clients.py
@@ -5,5 +5,5 @@ from sqlalchemy.engine.base import Connection
 def get_mysql_financialdata_conn() -> Connection:
     address = 'mysql+pymysql://root:root@139.162.60.41:3306/financialdata'
     enginer = create_engine(address)
-    connect = engine.connect()
+    connect = enginer.connect()
     return connect
