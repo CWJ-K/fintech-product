6f4045e0523f7d8cebbce44dce7a89822c01f504
CWJ-K <wanju412@gmail.com>
2022-06-15 01:55:25 +0000

    fix(pipfile, client): install apsscheduler, db con

--- src/backend/db/clients.py
+++ src/backend/db/clients.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine, engine
 from sqlalchemy.engine.base import Connection
+from src import config
 
 
 def get_mysql_invest_connection() -> Connection:
-    address = 'mysql+pymysql://user:user@localhost:3306/invest'
+    address = (
+        f'mysql+pymysql://{config.MYSQL_DATA_USER}:{config.MYSQL_DATA_PASSWORD}'
+        f'@{config.MYSQL_DATA_HOST}:{config.MYSQL_DATA_PORT}/{config.MYSQL_DATA_DATABASE}'
+    )
     engineer = create_engine(address)
     connect = engineer.connect()
     return connect
